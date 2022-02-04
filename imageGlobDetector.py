a43b49f11f95ead1f00bd25e797873af2f8f4b14
Antony Naumovic <59066658+antonynaumovic-alt@users.noreply.github.com>
2022-02-04 11:13:27 +0000

    Variable Naming convention to satisfy PEP8

--- imageGlobDetector.py
+++ imageGlobDetector.py
@@ -4,37 +4,42 @@ import cv2
 import glob
 import configparser
 
+
 def show_image(image):
-    cv2.imshow('image',image)
+    cv2.imshow('image', image)
     c = cv2.waitKey()
-    if c >= 0 : return -1
+    if c >= 0:
+        return -1
     return 0
 
-def verify_images(imageArray, maskPath, threshold):
-    matchesIter = imageArray.copy()
-    for image in matchesIter:
-        if not verify_image(maskPath + "/" + image, threshold):
-            imageArray.remove(image)
+
+def verify_images(image_array, mask_path, threshold):
+    matches_iter = image_array.copy()
+    for image in matches_iter:
+        if not verify_image(mask_path + "/" + image, threshold):
+            image_array.remove(image)
             print(f"Removing: {image} for not enough of object")
-    return imageArray
+    return image_array
 
 
-def set_config(renderPath, maskPath, outputPath):
+def set_config(render_path, mask_path, output_path):
     config = configparser.ConfigParser()
     with open("cookies.ini", "w+") as ini:
         config.add_section('paths')
-        config.set('paths', 'image', renderPath)
-        config.set('paths', 'mask', maskPath)
-        config.set('paths', 'output', outputPath)
+        config.set('paths', 'image', render_path)
+        config.set('paths', 'mask', mask_path)
+        config.set('paths', 'output', output_path)
         config.write(ini)
+
 
 def get_config():
     config = configparser.ConfigParser()
     config.read('cookies.ini')
-    imagesPath = config['paths']['image']
-    maskPath = config['paths']['mask']
-    outputPath = config['paths']['output']
-    return imagesPath, maskPath, outputPath
+    images_path = config['paths']['image']
+    mask_path = config['paths']['mask']
+    output_path = config['paths']['output']
+    return images_path, mask_path, output_path
+
 
 def config_setup():
     config = configparser.ConfigParser()
@@ -47,43 +52,33 @@ def config_setup():
             config.write(ini)
 
 
-def get_images_from_path(imagePath):
+def get_images_from_path(image_path):
     renders = []
-    masks = []
-    for foundImage in glob.glob(f'{imagePath}/*.png'):
+    for foundImage in glob.glob(f'{image_path}/*.png'):
         renders.append(os.path.basename(foundImage))
     return renders
 
 
-def process_image(renderPath, maskPath, outputPath):
-    im = cv2.imread(maskPath)
-    #cv2.imshow("Mask", im)
-    im_render = cv2.imread(renderPath)
-    #cv2.imshow("Colour", im_render)
-    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-
-    retries, threshold = cv2.threshold(imgray, 120, 255, 0)
+def process_image(render_path, mask_path, output_path):
+    im = cv2.imread(mask_path)
+    image_render = cv2.imread(render_path)
+    image_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    retries, threshold = cv2.threshold(image_gray, 120, 255, 0)
     contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    #img = cv2.drawContours(im_render, contours, -1, (255,255,0), 2)
-
     areas = [cv2.contourArea(cont) for cont in contours]
     max_index = np.argmax(areas)
     cnt = contours[max_index]
     x,y,w,h = cv2.boundingRect(cnt)
-    #cv2.rectangle(im_render,(x,y),(x+w,y+h),(0,255,0),2)
-    crop_img = im_render[y:y + h, x:x + w]
-    #show_image(crop_img)
-    cv2.imwrite(outputPath+"/"+os.path.basename(renderPath),crop_img)
+    crop_img = image_render[y:y + h, x:x + w]
+    cv2.imwrite(output_path + "/" + os.path.basename(render_path), crop_img)
 
 
-def verify_image(maskPath, threshold):
-    img = cv2.imread(maskPath, cv2.IMREAD_GRAYSCALE)
-    whitePixels = np.sum(img == 255)
-    totalPixels = img.shape[0]*img.shape[1]
-    whitePercent = (whitePixels/totalPixels) * 100
-    if whitePercent < threshold:
+def verify_image(mask_path, threshold):
+    img = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+    white_pixels = np.sum(img == 255)
+    total_pixels = img.shape[0]*img.shape[1]
+    white_percent = (white_pixels/total_pixels) * 100
+    if white_percent < threshold:
         return False
-    else:
-        return True
+    return True
 
