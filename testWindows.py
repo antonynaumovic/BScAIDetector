e98f7cdc9bb88653213b6f0f9ce48278bf8798c3
Antony Naumovic <59066658+antonynaumovic-alt@users.noreply.github.com>
2022-03-31 19:33:49 +0100

    Windows Testing for Show and Tell

---
+++ testWindows.py
@@ -0,0 +1,20 @@
+from detectron2.engine import DefaultPredictor
+import os
+import pickle
+import glob
+from utils import *
+
+config_save_path = r"C:\Users\anton\Documents\DetectronDataset/cfg.pickle"
+with open(config_save_path, 'rb') as conf:
+    config = pickle.load(conf)
+output_path = r"C:\Users\anton\Documents\DetectronDataset\output"
+config.MODEL.WEIGHTS = os.path.join(output_path, "model_final.pth")
+config.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+config.MODEL.DEVICE = "cpu"
+predictor = DefaultPredictor(config)
+
+image_path = r"C:\Users\anton\Documents\DetectronDataset\evaluation"
+eval = []
+for foundImage in glob.glob(f'{image_path}/*.png'):
+    eval.append(os.path.basename(foundImage))
+    vis_image(foundImage, predictor)
