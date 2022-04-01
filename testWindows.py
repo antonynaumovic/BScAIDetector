6bdd17c9791f58507b182749fb4416e7f1329933
Antony Naumovic <59066658+antonynaumovic-alt@users.noreply.github.com>
2022-04-01 06:33:53 +0100

    Added back removed config

--- testWindows.py
+++ testWindows.py
@@ -9,7 +9,7 @@ with open(config_save_path, 'rb') as conf:
     config = pickle.load(conf)
 output_path = r"C:\Users\anton\Documents\DetectronDataset\output"
 config.MODEL.WEIGHTS = os.path.join(output_path, "model_final.pth")
-config.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+config.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.85
 config.MODEL.DEVICE = "cpu"
 predictor = DefaultPredictor(config)
 
